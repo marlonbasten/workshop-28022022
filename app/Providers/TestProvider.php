623452cb407ed1fcf5457bb13fe714c427430465
Marlon Basten <mbasten@byte5.de>
2022-02-28 16:48:49 +0100

    feat: Added auth

---
+++ app/Providers/TestProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class TestProvider extends \Illuminate\Foundation\Support\Providers\RouteServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
